d728b5d73d334b9e3d9c3440ef0bae6aec890f19
hanzhiyu1995 <116212016+hanzhiyu1995@users.noreply.github.com>
2023-04-26 14:12:11 +0200

    Add files via upload
    update tast1

---
+++ utils.py
@@ -0,0 +1,31 @@
+import os
+import csv
+
+def createCsv(folderPath):
+    dataList = []
+    labelList = []
+    lengthOfFolderPath = len(folderPath)
+    for root, dirs, files in os.walk(folderPath):
+        for filename in files:
+            dataList.append(os.path.join(root, filename))
+            labelList.append(root[lengthOfFolderPath+1:])
+    totalNumberOfData = len(dataList)
+
+    header = ['Data', 'Label']
+    # open the file in the write mode
+    with open("output.csv", mode="w", newline="", encoding="utf-8") as csvFile:
+        # create the csv writer
+        writer = csv.writer(csvFile)
+        writer.writerow(header)
+        for i in range(totalNumberOfData):
+            writer.writerow([dataList[i], labelList[i]])
+
+    
+
+
+
+
+
+        
+
+
