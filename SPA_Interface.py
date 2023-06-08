a02f099194e6c5955daaf1b296ad19d833bb8df4
LillianKuhnn <lillian.kuhn@gmail.com>
2023-06-09 00:16:48 +0200

    added end page and integrated default batchSize

--- SPA_Interface.py
+++ SPA_Interface.py
@@ -25,6 +25,8 @@ import gradio as gr
 from Dataset import *
 import json
 import class_katalog
+from threading import Thread
+import time
 
 class SPA_Interface():
     
@@ -36,9 +38,10 @@ class SPA_Interface():
         self.uID = None
         self.batchSize = self.defaultBatchSize
         self.labels, self.indexList, self.sourceList, self.topTenList, self.labelList= self.__loadDataFromModel(self.batchSize)
+        self.finished = False
         
     def __loadDataFromModel(self,batchSize):
-        batch, batch3dim, indexList, sourceList, labelList = createRandomBatch(batchSize)
+        batch, batch3dim, indexList, sourceList, labelList = createRandomBatch(batchSize, self.uID)
         global modelName
         modelName = "convnext_tiny"
         model = get_new_model(modelName, not_original=True)
@@ -46,7 +49,7 @@ class SPA_Interface():
             ckpt = torch.load('convnext_tiny_cvst_clean.pt', map_location='cpu')
             ckpt = {k.replace('module.', ''): v for k, v in ckpt.items()}
             model.load_state_dict(ckpt)
-        samples = feedModel(batch) 
+        samples = feedModel(batch, model) 
         topTenList = findLabels(samples, 10)
         labels = []
         # just for test purpose
@@ -122,41 +125,64 @@ class SPA_Interface():
     
     def __incrementIndex(self):
         self.index +=1
-        if (self.index >= self.batchSize):
-            sys.exit(0)
+        #if (self.index >= self.batchSize):
+            #sys.exit(0)
             
     
     # user selected IID
     def __selectIID(self):#userId, iList, allL, bSize, loop):
-    # update json
-        self.__incrementIndex()
-        return gr.update(value=self.sourceList[self.index])
+        if self.index >= self.batchSize-1:
+            return gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(visible=True),gr.update(visible=True),gr.update(visible=True)
+        else:
+            self.__incrementIndex()
+            return gr.update(value=self.sourceList[self.index]),gr.update(visible=True),gr.update(visible=True),gr.update(visible=True),gr.update(visible=False),gr.update(visible=False),gr.update(visible=False)
 
     # user selected OOD    
     def __selectOOD(self):
-        self.__incrementIndex()
-        return gr.update(value=self.sourceList[self.index])
+        if self.index >= self.batchSize-1:
+            return gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(visible=True),gr.update(visible=True),gr.update(visible=True)
+        else:
+            self.__incrementIndex()
+            return gr.update(value=self.sourceList[self.index]),gr.update(visible=True),gr.update(visible=True),gr.update(visible=True),gr.update(visible=False),gr.update(visible=False),gr.update(visible=False)
 
     # user selected abstinent
     def __selectAbstinent(self):
-        self.__incrementIndex()
-        return gr.update(value=self.sourceList[self.index])
+        if self.index >= self.batchSize-1:
+            return gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(visible=True),gr.update(visible=True),gr.update(visible=True)
+        else:
+            self.__incrementIndex()
+            return gr.update(value=self.sourceList[self.index]),gr.update(visible=True),gr.update(visible=True),gr.update(visible=True),gr.update(visible=False),gr.update(visible=False),gr.update(visible=False)
     
     def submitHandler(self,batchSize):
         if not self.loggedIn:
             self.loggedIn=True
-            self.batchSize = int(batchSize)
-            return gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(visible=True),gr.update(visible=True),gr.update(visible=True),gr.update(visible=True)
+            try:            
+                self.batchSize = int(batchSize)
+            finally:
+                return gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(visible=True),gr.update(visible=True),gr.update(visible=True),gr.update(visible=True)
     
         
         return None
+    
+    def saveData(self):
+        # update json
+        return gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(value="Evaluation got saved. You can now exit the execution by clicking 'Close' once and then you can close the tab."),gr.update(visible=False),gr.update(visible=True)
+    
+    def lastPage(self):
+        self.finished = True
+        return gr.update(visible=False), gr.update(visible=False), gr.update(visible=False), gr.update(visible=False), gr.update(visible=False), gr.update(visible=False), gr.update(visible=False)
+    
+    def sysExit(self):
+        while self.finished == False:
+            time.sleep(1)
+        thread_1._stop()
         
     def interface(self):
         with gr.Blocks() as demo:
             
             # login
             with gr.Row() as title:
-                text= gr.Markdown(value="Please insert your username or your user ID and your desired batchsize")
+                text= gr.Markdown(value="Please insert your username or your user ID and your desired batchsize. If you don´t enter a batchsize a batch of 10 samples will automatically be generated.")
             with gr.Row() as login1:
                 username = gr.Textbox(label="Username")
             with gr.Row()as login2:
@@ -179,17 +205,32 @@ class SPA_Interface():
                 buttonIID = gr.Button("IID")
                 buttonABS = gr.Button("abstinent")
                 
-                buttonOOD.click(self.__selectOOD, inputs=None, outputs=image)
-                buttonIID.click(self.__selectIID, inputs=None, outputs=image)
-                buttonABS.click(self.__selectAbstinent, inputs=None, outputs=image)
+            # end page
+            with gr.Row(visible=False) as end1:
+                text1 = gr.Markdown(value="Evaluation is over. Thanks for your help.")
+            with gr.Row(visible=False) as end2:
+                text2 = gr.Markdown(value="Please use the confirm-button to save your evaluation before you click 'Close'.")
+                buttonConfirm = gr.Button("Confirm")
+            with gr.Row(visible=False) as end3:
+                buttonClose = gr.Button("Close")
                 
-            image.change(self.handleImageInput, inputs=None, outputs=[labelOne, description,labelRest])
+            image.change(self.handleImageInput, inputs=None, outputs=[labelOne, description,labelRest]) 
             submitButton.click(self.submitHandler, inputs=batchSize, outputs=[title,login1,login2,login3,classifier1,classifier2, classifier3, classifier4])
-
+            buttonOOD.click(self.__selectOOD, inputs=None, outputs=[image,classifier2, classifier3, classifier4, end1, end2, end3])
+            buttonIID.click(self.__selectIID, inputs=None, outputs=[image,classifier2, classifier3, classifier4, end1, end2, end3])
+            buttonABS.click(self.__selectAbstinent, inputs=None, outputs=[image,classifier2, classifier3, classifier4, end1, end2, end3])
+            buttonConfirm.click(self.saveData, inputs=None, outputs=[classifier1,classifier2, classifier3, classifier4, text1, end2, end3])
+            buttonClose.click(self.lastPage, inputs=None, outputs=[classifier1,classifier2, classifier3, classifier4, end1, end2, end3])
         
         demo.launch()
-    
-    
 
 SPA = SPA_Interface()
-SPA.interface()
+
+thread_1 = Thread(target=SPA.interface)
+thread_2 = Thread(target=SPA.sysExit)
+
+thread_1.start()
+thread_2.start()
+
+#SPA = SPA_Interface()
+#SPA.interface()
