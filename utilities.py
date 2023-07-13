2ef37922ae741b4bfeed6e8faf8b912188211220
Hanzhiyu1995 <116212016+zhiyuhan1995@users.noreply.github.com>
2023-07-13 21:43:19 +0200

    Add files via upload

--- utilities.py
+++ utilities.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import requests
 
 def createAnnotation(folderPath):
     dataList = []
@@ -19,3 +20,35 @@ def createAnnotation(folderPath):
         writer.writerow(header)
         for i in range(totalNumberOfData):
             writer.writerow([dataList[i], labelList[i]])
+
+def getRow(row_number, file_path = 'output.csv'):
+    with open(file_path, 'r', newline='') as file:
+        reader = csv.DictReader(file)
+        rows = list(reader)
+        
+        assert not (row_number < 0 or row_number >= len(rows))
+        row = rows[row_number]
+        img = row['Data'].split('\\')[-1]
+        return {'label':row['Label'], 'img':img}
+
+def createUrl(row_number):
+    dict = getRow(row_number)
+    imgName = dict['img']
+    url = f"https://nc.mlcloud.uni-tuebingen.de/index.php/s/TgSK4n8ctPbWP4K/download?path=%2F{dict['label']}&files={dict['img']}"
+    return url, imgName
+
+def fetchOneImg(imgIndex):
+    url, img = createUrl(imgIndex)
+    response = requests.get(url)
+    # filename = imgFolder + '/' + img
+    # with open(filename, 'wb') as file:
+    #     file.write(response.content)
+    return response.content
+
+def fetchBatch(indexList):
+    dirName = 'imgBatch'
+    if not os.path.exists(dirName):
+        os.makedirs(dirName)
+    for index in indexList:
+        fetchOneImg(index, dirName)
+    
